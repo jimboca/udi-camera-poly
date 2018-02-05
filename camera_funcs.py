9629c17f2dbef45ae5d0bf834a25e594ac4b6973
Jim Searle <jimsearle@gmail.com>
2018-02-05 15:21:21 -0800

    Only allow utf-8 characters in node names

--- camera_funcs.py
+++ camera_funcs.py
@@ -90,7 +90,10 @@ def str2int(value):
 
 # Removes invalid charaters for ISY Node description
 def get_valid_node_name(name):
-    # Remove <>`~!@#$%^&*(){}[]?/\;:"'` characters from names
+    # Only allow utf-8 characters
+    #  https://stackoverflow.com/questions/26541968/delete-every-non-utf-8-symbols-froms-string
+    name = bytes(name, 'utf-8').decode('utf-8','ignore')
+    # Remove <>`~!@#$%^&*(){}[]?/\;:"'` characters from name
     return re.sub(r"[<>`~!@#$%^&*(){}[\]?/\\;:\"']+", "", name)
 
 def get_server_data(logger):
